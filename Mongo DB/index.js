41d43a77e51209d2d3c2c071e5128960a38c6611
kumar1310aryan <kumar1310aryan@gmail.com>
2024-06-22 11:04:09 +0530

    Insert Many

--- Mongo DB/index.js
+++ Mongo DB/index.js
@@ -22,28 +22,38 @@ const User = mongoose.model("User", userSchema);
 const Employee = mongoose.model("Employee", userSchema);
 
 //insertion (One)
-const user1 = new User({
-  name: "Aryan",
-  email: "kumar1310aryan@gmail.com",
-  age: 19,
+// const user1 = new User({
+//   name: "Aryan",
+//   email: "kumar1310aryan@gmail.com",
+//   age: 19,
+// });
+
+// user1.save();
+
+// const employee1 = new Employee({
+//   name: "AryanKumar",
+//   email: "kumararyan@gmail.com",
+//   age: 19,
+// });
+
+// employee1
+//   .save()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//inserting Multiple
+
+User.insertMany([
+  { name: "Xyz", email: "xyz@gmail.com", age: 199 },
+  { name: "Yzx", email: "yzx@gmail.com", age: 299 },
+  { name: "Zxy", email: "zxy@gmail.com", age: 399 },
+]).then((res) => {
+  console.log(res);
 });
 
-user1.save();
-
-const employee1 = new Employee({
-  name: "AryanKumar",
-  email: "kumararyan@gmail.com",
-  age: 19,
-});
-
-employee1
-  .save()
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-
-  //
+//Mongoose uses Operation Buffering :- It means Mongoose lets you start using models immediately, without waiting for mogoose to establish a connection to MongoDB.
+//
