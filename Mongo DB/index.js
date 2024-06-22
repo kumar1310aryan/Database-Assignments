cc9a2eb2a2026492109c04f2c5185cf5c1fd7607
kumar1310aryan <kumar1310aryan@gmail.com>
2024-06-22 10:32:56 +0530

    Mongo DB

--- Mongo DB/index.js
+++ Mongo DB/index.js
@@ -10,8 +10,13 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
+//schema creation
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   age: Number,
 });
+
+//creating collection in database
+// const User=mongoose.model("User", userSchema);
+const Employee=mongoose.model("Employee", userSchema);
