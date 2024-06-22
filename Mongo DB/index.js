b7d0749577c05a1dab8e91fc55a228f9180848a3
kumar1310aryan <kumar1310aryan@gmail.com>
2024-06-22 10:52:42 +0530

    Insertion in DB

--- Mongo DB/index.js
+++ Mongo DB/index.js
@@ -18,5 +18,32 @@ const userSchema = new mongoose.Schema({
 });
 
 //creating collection in database
-// const User=mongoose.model("User", userSchema);
-const Employee=mongoose.model("Employee", userSchema);
+const User = mongoose.model("User", userSchema);
+const Employee = mongoose.model("Employee", userSchema);
+
+//insertion (One)
+const user1 = new User({
+  name: "Aryan",
+  email: "kumar1310aryan@gmail.com",
+  age: 19,
+});
+
+user1.save();
+
+const employee1 = new Employee({
+  name: "AryanKumar",
+  email: "kumararyan@gmail.com",
+  age: 19,
+});
+
+employee1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+  //
